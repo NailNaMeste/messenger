e717e48f3d0d040dfd6706575fd7c893e423eeb2
ValarMorghulisSUSU <lenovoa736@gmail.com>
2020-12-09 20:06:34 +0500

    lesson2

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from sanic import Sanic
-from sanic.request import Request
-from sanic.response import HTTPResponse, json
+from transport.sanic.configure_sanic import configure_app
 
 
-app = Sanic(__name__)
+def main():
+    app = configure_app()
 
-
-@app.route('/', methods=['POST', 'GET'])
-async def health_endpoint(request: Request) -> HTTPResponse:
-
-    response = {
-        'hello': 'world'
-    }
-
-    if 'POST' in request.method:
-        response.update(request.json)
-
-    return json(body=response, status=200)
-
-
-app.run(
-    host='localhost',
-    port=8000,
-)
+    app.run(
+        host='localhost',
+        port=8000,
+    )
